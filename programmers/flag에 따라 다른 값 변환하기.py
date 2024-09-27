04badcd69889e3c3aa07c569bbb468d1983821c9
manggee <manggee@manggeeui-MacBookPro.local>
2024-09-27 19:43:57 +0900

    flag에 따라 다른 값 변환하기

---
+++ programmers/flag에 따라 다른 값 변환하기.py
@@ -0,0 +1,16 @@
+a = -4
+b = 7
+flag = True
+
+
+def solution(a, b, flag):
+    if flag:
+        return a + b
+    else:
+        return a - b
+
+
+print(solution(a, b, flag))
+
+# def soulution(a, b, flag):
+#     return a + b if flag else a - b
