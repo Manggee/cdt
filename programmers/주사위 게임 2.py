cdd1ac39aa82598087d3c29a2a18132680e43745
mangG907 <mang.gee97@gmail.com>
2024-09-30 00:13:15 +0900

    주사위 게임 2

--- programmers/주사위 게임 2.py
+++ programmers/주사위 게임 2.py
@@ -3,25 +3,7 @@ b = 4
 c = 4
 
 
-# def solution(a, b, c):
-#     answer = 0
-
-#     result = a + b + c
-#     result2 = a**2 + b**2 + c**2
-#     result3 = a**3 + b**3 + c**3
-
-#     if a != b != c != a:
-#         answer = result
-#     elif a == b == c:
-#         answer = (result) * (result2) * (result3)
-#     else:
-#         answer = (result) * (result2)
-#     return answer
-
-
-# print(solution(a, b, c))
-
-
+# 내가 생각한 답변
 def solution(a, b, c):
     answer = 0
     if a == b and b == c:
@@ -33,6 +15,26 @@ def solution(a, b, c):
     else:
         answer = a + b + c
 
+    return answer
+
+
+print(solution(a, b, c))
+
+
+# 다른 사람의 풀이
+def solution(a, b, c):
+    answer = 0
+
+    result = a + b + c
+    result2 = a**2 + b**2 + c**2
+    result3 = a**3 + b**3 + c**3
+
+    if a != b != c != a:
+        answer = result
+    elif a == b == c:
+        answer = (result) * (result2) * (result3)
+    else:
+        answer = (result) * (result2)
     return answer
 
 
