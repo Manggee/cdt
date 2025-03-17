3bb588c46c6b64424a0564b3378e9c9b58d9ce09
mangG907 <mang.gee97@gmail.com>
2025-03-17 14:31:05 +0900

    오답 완료

---
+++ programmers/Lv0/가까운 1 찾기.py
@@ -0,0 +1,10 @@
+arr = [1,1,1,1,0]
+idx = 3
+
+def solution(arr, idx):
+    for i in range(idx, len(arr)):
+        if arr[i] == 1:
+            return i
+    else:
+        return -1
+print(solution(arr, idx))
