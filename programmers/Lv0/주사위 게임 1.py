3dcc136aef333bc1ac2e8c043287fcb5d2c7d5a8
mangG907 <mang.gee97@gmail.com>
2025-07-08 15:55:46 +0900

    풀이 완료

---
+++ programmers/Lv0/주사위 게임 1.py
@@ -0,0 +1,11 @@
+def solution(a, b):
+    if (a + b) % 2 == 0:
+        if (a % 2 == 0) and (b % 2 == 0):
+            answer = abs(a-b)
+        else:
+            answer = a ** 2 + b ** 2
+    else:
+        answer = 2 * (a + b)
+        return answer
+    return answer
+print(solution(2,4))
