7dd137bd6ba7de41694ad998358b4567e8c9fc26
mangG907 <mang.gee97@gmail.com>
2025-07-08 15:28:44 +0900

    오답 완료(다음 코테에서는 자만하지 않기..)

--- programmers/PCCE/test3.py
+++ programmers/PCCE/test3.py
@@ -3,7 +3,7 @@ arr = [[50,170],[70,160],[80,185]]
 def solution(arr):
     for j in range(1,len(arr)):
         for i in range(j-1, -1, -1):
-            if arr[i][0]+arr[i][1] < arr[i+1][0]+arr[i+1][1]:
+            if arr[i][0]+arr[i][1] > arr[i+1][0]+arr[i+1][1]:
                 arr[i],arr[i+1] = arr[i+1],arr[i]
     return arr
 
