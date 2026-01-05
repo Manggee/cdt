b4b87c11ceb0e95040e81bb2768e190c86db0ae3
manggee <mang.gee97@gmail.com>
2026-01-05 20:36:20 +0900

    문제풀이 완료

--- programmers/java/Lv0/Q1.java
+++ programmers/java/Lv0/Q1.java
@@ -1,4 +1,22 @@
 package Lv0;
 
 public class Q1 {
+    public int solution(int[] args) {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] < 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Q1 s = new Q1();
+
+        int[] a = {12, 4, 15, 46, 38, -2, 15};
+        int[] b = {13, 22, 53, 24, 15, 6};
+
+        System.out.println(s.solution(a));
+        System.out.println(s.solution(b));
+    }
 }
--- programmers/java/Lv0/Q2.java
+++ programmers/java/Lv0/Q2.java
@@ -1,4 +1,18 @@
 package Lv0;
 
 public class Q2 {
+    public int solution(int num, int n) {
+        if (num % n == 0) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Q2 s = new Q2();
+        System.out.println(s.solution(98, 2));
+        System.out.println(s.solution(34, 3));
+    }
 }
